660c129173e3feb3e16b23a3cb6229cbf35812d1
javier <js@silveriofunk.com>
2014-10-08 15:02:25 +0100

    post model created. database and tests updated

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'posts page' do
+describe 'posts' do
   context 'no posts have been added' do
     it 'should display a prompt to add posts' do
       visit '/posts'
@@ -8,4 +8,18 @@ describe 'posts page' do
       expect(page).to have_link 'Add post'
     end
   end
+
+  context 'posts have been added' do
+    before do
+      Post.create(name: 'My cat')
+    end
+
+    it 'should display them' do
+      visit '/posts'
+      expect(page).to have_content "A photo of my cat"
+      expect(page).not_to have_content "Posts pending"
+    end
+  end
+
+
 end
